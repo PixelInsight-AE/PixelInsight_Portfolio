ae91aa5f35abe83e37296f0cc7eb8ea3c8df49bc
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-09-22 21:48:16 -0600

    contact med and up
    Co-authored-by: Arno Luyckx <BlocksOnChainCode@users.noreply.github.com>

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,39 +9,42 @@ import ContactForm from "../shared/ContactForm";
 import GetAQuote from "../shared/GetAQuote";
 const Contact = () => {
   return (
-    <div className="contact">
+    <>
       <Helmet>
         <title>Pixel - Contact</title>
       </Helmet>
       <Header />
-      <div className="contact__top-wrapper">
-        <h1>Connect with Pixel Insight: Your Questions, Our Answers!</h1>
-        <img
-          className="conatct__phone-mobile"
-          src="/assets/svg/phone.svg"
-          alt=""
-        />
+      <div className="contact">
+        <div className="contact__top-wrapper">
+          <h1>Connect with Pixel Insight: Your Questions, Our Answers!</h1>
+          <img
+            className="contact__phone-mobile"
+            src="/assets/svg/phone.svg"
+            alt=""
+          />
+        </div>
+        <div className="contact__form-wrapper">
+          <ContactForm />
+          <img
+            className="contact__phone-desktop"
+            src="/assets/svg/phone.svg"
+            alt=""
+          />
+        </div>
+        <p className="contact__text">
+          At Pixel Insight, we value your input, questions, and feedback.
+          Whether you're interested in our services, have inquiries about
+          ongoing projects, or simply want to say hello, we're here to listen
+          and assist you. Our team of experts is just a message away, ready to
+          provide the information and support you need. Don't hesitate to fill
+          out the form below or reach out through our provided contact details.
+          We look forward to connecting with you and exploring the exciting
+          possibilities ahead.
+        </p>
+        <GetAQuote />
+        <Footer />
       </div>
-      <div>
-        <ContactForm />
-        <img
-          className="contact__phone-desktop"
-          src="/assets/svg/phone.svg"
-          alt=""
-        />
-      </div>
-      <p className="contact__text">
-        At Pixel Insight, we value your input, questions, and feedback. Whether
-        you're interested in our services, have inquiries about ongoing
-        projects, or simply want to say hello, we're here to listen and assist
-        you. Our team of experts is just a message away, ready to provide the
-        information and support you need. Don't hesitate to fill out the form
-        below or reach out through our provided contact details. We look forward
-        to connecting with you and exploring the exciting possibilities ahead.
-      </p>
-      <GetAQuote />
-      <Footer />
-    </div>
+    </>
   );
 };
 export default Contact;
