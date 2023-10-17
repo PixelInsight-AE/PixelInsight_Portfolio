ce5cbade665b9af00cd063e9060d61b02c12108c
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-10-17 12:59:05 -0600

    tech expertise

--- src/components/home/TechExpertise.jsx
+++ src/components/home/TechExpertise.jsx
@@ -1,17 +1,22 @@
 const TechExpertise = () => {
   return (
     <section className="tech-expertise">
-      <h2 className="tech-expertise__title">Technology Expertise</h2>
+      <section className="tech-expertise__header">
+        <h2 className="tech-expertise__title">Technology Expertise</h2>
+        <p className="tech-expertise__subtitle">
+          Here at Pixel, we leverage the most advanced technologies, and push
+          the boundaries of what is possible.
+        </p>
+      </section>
       <div className="tech-expertise__wrapper">
         <article className="tech-expertise__card left">
           <section className="tech-expertise__card-header">
-            <h3>Front-end</h3>
+            <h2>Front-end</h2>
             <img src="/assets/svg/circle.svg" alt="" />
           </section>
           <p>
-            Elevate your online presence with our expert front-end design
-            solutions. We create modern, user-friendly interfaces that captivate
-            your audience.
+            Elevate your online presence with our friendly interfaces that
+            captivate your audience.
           </p>
           <i>
             HTML, CSS, Sass, React, Redux, JavaScript, Bootstrap, FrameWorks,
@@ -20,12 +25,12 @@ const TechExpertise = () => {
         </article>
         <article className="tech-expertise__card right">
           <section className="tech-expertise__card-header">
-            <h3>Back-end</h3>
+            <h2>Back-end</h2>
             <img src="/assets/svg/triangle.svg" alt="" />
           </section>
           <p>
-            Elevate your digital presence with our powerful backend solutions
-            for seamless performance and security.
+            Powerful backend solutions for seamless performance and security.
+            Take your digital presence to the next level.
           </p>
           <i>
             Database Management, API Development, Authentication Systems, Server
@@ -35,27 +40,23 @@ const TechExpertise = () => {
         </article>
         <article className="tech-expertise__card left">
           <section className="tech-expertise__card-header">
-            <h3>Design</h3>
+            <h2>Design</h2>
             <img src="/assets/svg/square.svg" alt="" />
           </section>
           <p>
-            At Pixel Insight, we blend creativity and functionality to craft
-            visually stunning, user-friendly designs that captivate and engage.
-            Our design solutions are not just aesthetically pleasing but also
-            purposeful, making your digital presence memorable and effective.
+            Blend creativity and functionality to craft visually stunning,
+            user-friendly designs that captivate and engage.
           </p>
           <i>Figma, Illustrator, Photoshop</i>
         </article>
         <article className="tech-expertise__card right">
           <section className="tech-expertise__card-header">
-            <h3>App Development</h3>
+            <h2>App Dev</h2>
             <img src="/assets/svg/star.svg" alt="" />
           </section>
           <p>
-            We're your mobile app maestros, transforming your unique concepts
-            into user-friendly, high-performance applications. From iOS to
-            Android, we create digital magic that resonates with your audience,
-            one download at a time.
+            Cross platform app development. We create digital magic that
+            resonates with your audience, one download at a time.
           </p>
           <i>React Native, Flutter</i>
         </article>
